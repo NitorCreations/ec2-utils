ac0c1fe0107e72aa9faace64844a8ac8fc5125bd
Pasi Niemi <pasi.niemi@nitor.com>
2019-06-06 14:14:02 +0300

    Sleep longer to avoid hitting snapshot rate limits

--- tests/volume-operations-snapshots.sh
+++ tests/volume-operations-snapshots.sh
@@ -4,7 +4,7 @@
 mkdir -p /mnt/testing-ss
 ec2 volume-from-snapshot ec2-testing-ss ec2-testing-ss /mnt/testing-ss 1 -i -t foo=bar -c 'test:instance'
 ec2 snapshot-from-volume ec2-testing-ss ec2-testing-ss /mnt/testing-ss -w -i -t foo=bar -c 'test:instance'
-sleep 5
+sleep 30
 ec2 snapshot-from-volume ec2-testing-ss ec2-testing-ss /mnt/testing-ss -w -i -t foo=bar -c 'test:instance'
 ec2 clean-snapshots ec2-testing-ss -t 1
 ec2 prune-snapshots -n ec2-testing-ss -M 1 -H 0 -d 0 -w 0 -m 0 -y 0
