01d7c602f7019c850e1a51b80bc16df958e946c4
Pasi Niemi <pasi.niemi@nitorcreations.com>
2019-06-14 00:40:57 +0300

    Tweaks for refactoring nameless-deploy-tools

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ from setuptools import setup
 from ec2_utils import CONSOLESCRIPTS
 
 setup(name='ec2-utils',
-      version='0.8',
+      version='0.9',
       description='Tools for using on an ec2 instance',
       url='http://github.com/NitorCreations/ec2-utils',
-      download_url='https://github.com/NitorCreations/ec2-utils/tarball/0.8',
+      download_url='https://github.com/NitorCreations/ec2-utils/tarball/0.9',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
