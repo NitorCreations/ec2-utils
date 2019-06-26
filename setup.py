7c98d05eb8bd40f440d8a2cc5af5e14dba3fa511
Pasi Niemi <pasi.niemi@nitorcreations.com>
2019-06-26 19:46:53 +0300

    Volume operation and logging fixes

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Nitor Creations Oy
+# Copyright 20.10 Nitor Creations Oy
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,10 +16,10 @@ from setuptools import setup
 from ec2_utils import CONSOLESCRIPTS
 
 setup(name='ec2-utils',
-      version='0.9',
+      version='0.10',
       description='Tools for using on an ec2 instance',
       url='http://github.com/NitorCreations/ec2-utils',
-      download_url='https://github.com/NitorCreations/ec2-utils/tarball/0.9',
+      download_url='https://github.com/NitorCreations/ec2-utils/tarball/0.10',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
