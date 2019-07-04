ccf9896fdf084a8d2280e3430a6056c065d8e2cf
Pasi Niemi <pasi.niemi@nitor.com>
2019-07-04 14:57:53 +0300

    Upgrade threadlocal-aws

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name='ec2-utils',
       ],
       install_requires=[
           'future',
-          'threadlocal-aws==0.2',
+          'threadlocal-aws==0.4',
           'awscli',
           'requests',
           'termcolor',
