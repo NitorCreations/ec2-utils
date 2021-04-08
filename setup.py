09e426e754b9ff8d61ff4317d8999dff772cbb98
Pasi Niemi <pasi.niemi@nitorcreations.com>
2021-04-08 11:38:50 +0300

    Default to creating encrypted volumes

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 20.24 Nitor Creations Oy
+# Copyright 20.25 Nitor Creations Oy
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,10 +16,10 @@ from setuptools import setup
 from ec2_utils import CONSOLESCRIPTS
 
 setup(name='ec2-utils',
-      version='0.24',
+      version='0.25',
       description='Tools for using on an ec2 instance',
       url='http://github.com/NitorCreations/ec2-utils',
-      download_url='https://github.com/NitorCreations/ec2-utils/tarball/0.24',
+      download_url='https://github.com/NitorCreations/ec2-utils/tarball/0.25',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
