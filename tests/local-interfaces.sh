4187df74de045226096b9705fdf9dec0b5e03921
Pasi Niemi <pasi.niemi@nitor.com>
2021-05-06 22:14:28 +0300

    Fix typo in name and add sleep

--- tests/local-interfaces.sh
+++ tests/local-interfaces.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
 
 ec2 register-private-dns ec2-utils-test.dev.nitor.zone dev.nitor.zone.
+sleep 10
 LOCAL_IP="$(dig +short ec2-utils-test.dev.nitor.zone)"
 ec2 list-local-interfaces  | egrep $LOCAL_IP
