2e59edb1bf6966cba96eea2278f42b31d0345aa9
Pasi Niemi <pasi.niemi@nitor.com>
2021-05-09 22:25:10 +0300

    Fix timezone handling in log printing

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 20.28 Nitor Creations Oy
+# Copyright 20.29 Nitor Creations Oy
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,10 +26,10 @@ elif sys.version_info[0] == 3:
         python2_or_3_test_deps.insert(0, "pytest")
 
 setup(name='ec2-utils',
-      version='0.28',
+      version='0.29',
       description='Tools for using on an ec2 instance',
       url='http://github.com/NitorCreations/ec2-utils',
-      download_url='https://github.com/NitorCreations/ec2-utils/tarball/0.28',
+      download_url='https://github.com/NitorCreations/ec2-utils/tarball/0.29',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
