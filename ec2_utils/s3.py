8e045a787c227e0a947d6a0645113ad4805745d2
Pasi Niemi <pasi.niemi@nitor.com>
2021-08-01 11:19:52 +0300

    Conflict with the argument name

--- ec2_utils/s3.py
+++ ec2_utils/s3.py
@@ -1,10 +1,10 @@
-from threadlocal_aws.resources import s3_Bucket as bucket
+from threadlocal_aws.resources import s3_Bucket as bucket_r
 from ec2_utils.utils import prune_array, delete_selected
 
 def prune_s3_object_versions(bucket=None, prefix="", ten_minutely=288, hourly=168,
                              daily=30, weekly=13, monthly=6, yearly=3, dry_run=False):
     time_func = lambda version: version.last_modified
-    versions = sorted(bucket(bucket).object_versions.filter(Prefix=prefix),
+    versions = sorted(bucket_r(bucket).object_versions.filter(Prefix=prefix),
                       key=time_func, reverse=True)
     #def prune_array(prunable, time_func, group_by_func, ten_minutely=None,
     #                hourly=None, daily=None, weekly=None, monthly=None, yearly=None,
