4e92b39c4b81a34a8b415ff239a2fb5620cb96dd
Pasi Niemi <pasi.niemi@nitorcreations.com>
2021-08-16 09:19:09 +0300

    Upgrade dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 20.33 Nitor Creations Oy
+# Copyright 20.34 Nitor Creations Oy
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,10 +22,10 @@ elif sys.version_info[0] == 3:
     python2_or_3_test_deps.insert(0, "pytest")
 
 setup(name='ec2-utils',
-      version='0.33',
+      version='0.34',
       description='Tools for using on an ec2 instance',
       url='http://github.com/NitorCreations/ec2-utils',
-      download_url='https://github.com/NitorCreations/ec2-utils/tarball/0.33',
+      download_url='https://github.com/NitorCreations/ec2-utils/tarball/0.34',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
